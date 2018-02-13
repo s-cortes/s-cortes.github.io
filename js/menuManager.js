1e5eb7038472b0b6c4fc1fb393781e2c6797344c
Santiago Cortes <s.cortes@uniandes.edu.co>
2018-02-12 21:20:06 -0500

    modificacion en la libreria de JQuery: cambio de HTTP a HTTPS en el cdn

--- js/menuManager.js
+++ js/menuManager.js
@@ -4,7 +4,7 @@ let vistaActual = "homeView";
 
 $(containerView).load(("html/".concat(vistaActual)).concat(".html"));
 
-$(".nav-link, .dropdown-item").click(function (eventData) {
+$(".item-displayer").click(function (eventData) {
     var elementId = String(eventData.target.id);
     
     if ((elementId.indexOf(view) !== -1) && (vistaActual !== elementId)) {
@@ -12,6 +12,7 @@ $(".nav-link, .dropdown-item").click(function (eventData) {
         elementId = ("html/".concat(elementId)).concat(".html");
         $(containerView).empty();
         $(containerView).load(elementId);
+        
     }
 });
 
